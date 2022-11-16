28249cf3bee8090edecdce9da6c8c4195eed3af3
test <test>
2022-11-16 07:27:27 +0300

    js

---
+++ script.js
@@ -0,0 +1,14 @@
+var modal = prompt("ismingizni kiriting","name")
+console.log("sizning isminjgiz "+modal);
+var modal = prompt("yoshingizni kiriting","yosh")
+console.log("sizning yoshingiz "+modal);
+var misol = prompt("misolni yeching 10-7=?")
+console.log("1-misol: 10-7=3 sizning javob: "+misol)
+var misol = prompt("misolni yeching 5+7=?")
+console.log("2-misol: 5+7=12 sizning javob: "+misol)
+var misol = prompt("misolni yeching 5*5=?")
+console.log("3-misol: 5*5=25 sizning javob: "+misol)
+var misol = prompt("misolni yeching 4/2=?")
+console.log("4-misol: 4/2=2 sizning javob: "+misol)
+
+alert("javobni bilish uchun consulga o't0shingiz mumkun")
